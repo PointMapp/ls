3bf9f1f641fb23dd8e7834bd23b2b58985e2f7b0
Pikachu <kaskrbaev7@gmail.com>
2023-02-13 14:05:46 +0600

    master

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
 
 let result;
 
+const number1 = prompt('Введите первую цифру: ');
 const operator = prompt('Выберите операцию ( любую +, -, * или / ): ');
-
-
-const number1 = parseFloat(prompt('Введите первую цифру: '));
-const number2 = parseFloat(prompt('Введите вторую цифру: '));
+const number2 = prompt('Введите вторую цифру: ');
 
 switch(operator) {
     case '+':
          result = number1 + number2;
-        console.log(`number1 + number2 = result`);
-        break;
+         alert(`${number1} + ${number2} = ${result}`);
+         break;
 
     case '-':
          result = number1 - number2;
-        console.log(`number1 - number2 = result`);
+        alert(`${number1} - ${number2} = ${result}`);
         break;
 
     case '*':
          result = number1 * number2;
-        console.log(`number1 * number2 = result`);
+        alert(`${number1} * ${number2} = ${result}`);
         break;
 
     case '/':
          result = number1 / number2;
-        console.log(`number1 / number2 = result`);
+        alert(`${number1} / ${number2} = ${result}`);
         break;
 
     default:
-        console.log('Недопустимый оператор');
+        alert('Недопустимый оператор');
         break;
 }
